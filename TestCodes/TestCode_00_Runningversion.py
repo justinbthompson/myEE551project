53f5d3aeda407bdd3ca05150af6276d910f0a26d
Justin Thompson <justin.b.thompson97@gmail.com>
2019-04-24 23:10:02 -0400

    Updated 2019-04-24 11:00 PM

--- TestCodes/TestCode_00_Runningversion.py
+++ TestCodes/TestCode_00_Runningversion.py
@@ -176,8 +176,8 @@ print("Welcome to the New York State Energy Data Analyzer") #Boot-up message
 #while loop to have program running continuously
 while True:
     date = user_input()         #Requests user input and date the user wants to examine
-    data = access_data(date)    #Collects the data based on user input, saves it as data
-    #analyze_data(data,date)     #Analyzes the data
+    #data = access_data(date)    #Collects the data based on user input, saves it as data
+    analyze_data(data,date)     #Analyzes the data
     analyze_data_method2(data, date)   #Quicker analysis function not in use, explained above
     print('')
 
