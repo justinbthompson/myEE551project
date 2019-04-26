b02d938e9ef4f92f83106d70533dbbdfcc6ff1c3
Justin Thompson <justin.b.thompson97@gmail.com>
2019-04-25 22:16:40 -0400

    Removed print from function to put in program

--- ProjectFinalCode/NewYorkStateEnergyConsumptionDataAnalyzer.py
+++ ProjectFinalCode/NewYorkStateEnergyConsumptionDataAnalyzer.py
@@ -9,8 +9,6 @@
 #--------------------------------------------------------------------------------
 #Import functions from function file
 from myEE551projectPowerDemand.ProjectFinalCode.project_functions import *
-
-
 #--------------------------------------------------------------------------------
 #Program
 
@@ -19,7 +17,15 @@ print("Welcome to the New York State Energy Data Analyzer") #Boot-up message
 while True:
     date = user_input()         #Requests user input and date the user wants to examine
     data = access_data(date)    #Collects the data based on user input, saves it as data
-    analyze_data(data,date)     #Analyzes the data
-    #analyze_data_method2(data, date)   #Quicker analysis function not in use, explained above
+    dt, dp, peakhour, peakhourS, peakminute, peakminuteS = analyze_data(data)     #Analyzes the data
+    #Print data found
+    print('Daily Total Energy Usage for ' + str(date[0]) + '-' + str(date[1]) + '-' + str(date[2]) + ': \n')
+    print(str(dt['All Sources']) + " MW \n")
+    print('Daily Total Energy Usage Per Energy Source: \n')
+    for x, y in dt.items():
+        if x not in 'Daily Total All Sources':
+            print(x + ": " + str(y) + " MW (" + str(dp[x]) + "%)")
+    print('\nPeak Energy Consumption occurred at ' + peakminuteS + ', with ' + str(peakminute) + ' MW')
+    print('Peak Energy Consumption over an hour occured at hour ' + peakhourS + ', with ' + str(peakhour) + ' MW')
     print('')
 
